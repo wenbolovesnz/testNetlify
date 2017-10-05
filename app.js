1d80dc8c7e82f434c6cccab2795eaa7003f000b9
Wenbo Li <wenbolovesnz@gmail.com>
2017-10-05 22:03:17 +1300

    tracking code working 1st version ready

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-	console.log( "ready!" );
+	$("#notify-btn").click(function(){
+		var email = $("#inputGroupSuccess1").val();
+		ga('send', 'event', 'UserEmail', email, 1);
+
+		$("#pre-submit").hide();
+		$("#after-submit").show();
+	})
 });
